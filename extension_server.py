82b0aec7f902b89739118b20aa1f59f34afad18b
Swastik Agarwala <agarwalaswastik@gmail.com>
2024-01-21 07:38:52 -0500

    Fixed camera-extension connection

--- extension_server.py
+++ extension_server.py
@@ -1,35 +1,101 @@
-import asyncio
 import websockets
-import multiprocessing as mp
-import pickle
 
-client = None
+import cv2
 
-async def send_message(message, extensionClient):
-    lock = mp.Lock()
-    with lock:
-        #clientInstance = pickle.loads(extensionClient)
-        pass
-    await clientInstance.send_message(message)
+from hand_detector import HandDetector
+from volume_control import getSpeakerOutputVolume, setVolume
+
+from action_detector import detectAction
+
+from distance_util import distanceBetweenLandmarks, setCamSize
+
+from constants import commands, actions
+
+import asyncio
+
+activeCommand = None
+
+def getCommand():
+    try:
+        activeCommand.acquire()
+        value = activeCommand.value
+        activeCommand.release()
+    except:
+        activeCommand.release()
+        value = ""
+    return value
+
+camWidth = 800
+camHeight = 500
+setCamSize(camWidth, camHeight)
+
+cameraIndex = 1
+waitKey = 10
+detector = HandDetector()
+
+queueMaxSize = 100
+landmarksQueue = []
+
+capture = cv2.VideoCapture(cameraIndex)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, camWidth)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, camHeight)
+
+async def send_message(message, client):
+    await client.send(message)
 
 async def new_client_connected(client_socket, path):
     print("New client connected!")
-    setClient(client_socket)
+    await startHandDetection(client_socket)
+
+async def startHandDetection(client_socket):
+    volume = 100
+
+    commandVal = getCommand()
+    action = None
+
+    while True:
+        success, image = capture.read()
+        image, landmarks = detector.drawAndGetLandmarks(image)
+
+        if len(landmarksQueue) >= queueMaxSize:
+            landmarksQueue.pop(0)
+
+        try:
+            landmarks = landmarks[0].landmark
+            landmarksQueue.append((landmarks[4], landmarks[8]))
+        except:
+            landmarksQueue.append(None)
+
+        newAction = detectAction(landmarksQueue)
+
+        if newAction != actions['Idle'] and newAction != action:
+            await send_message(str(newAction), client_socket)
+
+        action = newAction
+
+        newCommandVal = getCommand()
+
+        if newCommandVal == commands['Volume'] and commandVal != commands['Volume']:
+            volume = getSpeakerOutputVolume()
+
+        commandVal = newCommandVal
+
+        if commandVal == commands['Volume'] and len(landmarksQueue) > 1 and landmarksQueue[-1]:
+            diff = distanceBetweenLandmarks(landmarksQueue[-1][0], landmarksQueue[-1][1]) - volume
+            deltaVolume = diff / 5 * 2
+            newVolume = round(volume + deltaVolume)
+            setVolume(newVolume)
+
+        cv2.imshow('img', image)
+        cv2.waitKey(waitKey)
 
 async def start_server():
     print("Starting server...")
     await websockets.serve(new_client_connected, "localhost", 12345)
 
-def setClient(client_socket):
-    global client
-    lock = mp.Lock()
-    with lock:
-        print(pickle.dumps(client_socket))
-        #client[:] = pickle.dumps(client_socket)
-
-def run_server(extensionClient):
-    global client
-    client = extensionClient
+def run_server(command):
+    global activeCommand
+    activeCommand = command
     event_loop = asyncio.get_event_loop()
     event_loop.run_until_complete(start_server())
     event_loop.run_forever()
--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 import multiprocessing as mp
-from multiprocessing import Value, Array
-
+from multiprocessing import Value
 import command_setter
-import camera
-
 from constants import commands
-
 from extension_server import run_server
 
 if __name__ == '__main__':
     command = Value('i', commands['Idle'])
-    extensionClient = Array('c', b'')
 
     # speechRecognitionProcess = mp.Process(name='speechRecognitionProcess', target=command_setter.startSpeechRecognition, args=(command,))
-    handDetectionProcess = mp.Process(name='handDetectionProcess', target=camera.startHandDetection, args=(command, extensionClient))
-
     # speechRecognitionProcess.start()
-    handDetectionProcess.start()
 
-    run_server(extensionClient)
+    run_server(command)
